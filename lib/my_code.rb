06d2918f0895ad7ebbb02df499a98423949052f5
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-12 01:53:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,7 +11,7 @@ def map(source_array)
   new_array
 end 
 
-def reduce(source_array,starting_value)
+def reduce(source_array,starting_value=0)
   sum = starting_point
   i = 0
   while i < source_array.length do
