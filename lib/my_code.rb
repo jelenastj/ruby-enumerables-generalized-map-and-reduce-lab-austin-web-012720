44d251597451cd24e0420f8dbc8a51140505e2bb
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-12 02:18:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,10 +6,13 @@ def map(source_array)
   
   while i < source_array.length do
   new_array.push(yield(source_array[i]))
+  
     i += 1
   end
   new_array
 end 
+
+
 
 def reduce(source_array,starting_value=0)
   value = starting_value
